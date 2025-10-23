e655c9aaa06e8df9521d546312875f961e45a494
TimN1987 <timothy.naish@gmail.com>
2025-10-23 14:16:50 +0100

    Completed sort descending method in C#.

--- src/C#/Sorting/InsertionSort.cs
+++ src/C#/Sorting/InsertionSort.cs
@@ -3,19 +3,20 @@ using System.Linq;
 
 namespace AlgorithmsLibrary.Sorting;
 
-public class InsertionSort
+public static class InsertionSort
 {
     /// <summary>
     /// Uses an insertion sort method to sort a collection of integers from smallest to largest.
     /// </summary>
-    public IEnumerable<int> SortAscending(IEnumerable<int> unsortedIntegers)
+    public static IEnumerable<int> SortAscending(IEnumerable<int> unsortedIntegers)
     {
         int[] sortingArray = unsortedIntegers.ToArray();
+        int length = sortingArray.Length;
 
-        if (sortingArray.Length == 0)
+        if (length == 0)
             return Enumerable.Empty<int>();
 
-        for (int j = 1; j < sortingArray.Length; j++)
+        for (int j = 1; j < length; j++)
         {
             int key = sortingArray[j];
             int i = j - 1;
@@ -35,17 +36,37 @@ public class InsertionSort
     /// <summary>
     /// Uses an insertion sort method to sort a collection of integers from largest to smallest.
     /// </summary>
-    public IEnumerable<int> SortDescending(IEnumerable<int> unsortedIntegers)
+    public static IEnumerable<int> SortDescending(IEnumerable<int> unsortedIntegers)
     {
-        return Enumerable.Empty<int>();
+        int[] sortingArray = unsortedIntegers.ToArray();
+        int length = sortingArray.Length;
+
+        if (length == 0)
+            return Enumerable.Empty<int>();
+
+        for (int j = 1; j < length; j++)
+        {
+            int key = sortingArray[j];
+            int i = j - 1;
+
+            while (i >= 0 && sortingArray[i] < key)
+            {
+                sortingArray[i + 1] = sortingArray[i];
+                i--;
+            }
+
+            sortingArray[i + 1] = key;
+        }
+
+        return sortingArray;
     }
 
-    public IEnumerable<T> SortAscending<T>(IEnumerable<T> unsortedNumbers)
+    public static IEnumerable<T> SortAscending<T>(IEnumerable<T> unsortedNumbers)
     {
         return Enumerable.Empty<T>();
     }
 
-    public IEnumerable<T> SortDescending<T>(IEnumerable<T> unsortedNumbers)
+    public static IEnumerable<T> SortDescending<T>(IEnumerable<T> unsortedNumbers)
     {
         return Enumerable.Empty<T>();
     }
--- tests/C#/SortingTests/InsertionSortTests.cs
+++ tests/C#/SortingTests/InsertionSortTests.cs
@@ -6,14 +6,23 @@ namespace Tests.SortingTests;
 
 public class InsertionSortTests
 {
-    private InsertionSort _sort = new InsertionSort();
-    
     [Theory]
     [MemberData(nameof(MemberDataSortAscendingIntegers))]
     public void SortAscending_IntegersInput_SortedIntegersReturned(IEnumerable<int> unsortedIntegers, IEnumerable<int> sortedIntegers)
     {
         //Arrange & Act
-        var result = _sort.SortAscending(unsortedIntegers);
+        var result = InsertionSort.SortAscending(unsortedIntegers);
+
+        //Assert
+        Assert.Equal(sortedIntegers, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(MemberDataSortDescendingIntegers))]
+    public void SortDescending_IntegersInput_SortedIntegersReturned(IEnumerable<int> unsortedIntegers, IEnumerable<int> sortedIntegers)
+    {
+        //Arrange & Act
+        var result = InsertionSort.SortDescending(unsortedIntegers);
 
         //Assert
         Assert.Equal(sortedIntegers, result);
@@ -35,5 +44,23 @@ public class InsertionSortTests
         yield return new object[] { new int[] { 5, 4, 3, 2, 1 }, new int[] { 1, 2, 3, 4, 5 } };
         yield return new object[] { new int[] { int.MaxValue, 0, int.MinValue }, new int[] { int.MinValue, 0, int.MaxValue } };
         yield return new object[] { Enumerable.Range(0, 100).Reverse(), Enumerable.Range(0, 100) };
+    }
+
+    public static IEnumerable<object[]> MemberDataSortDescendingIntegers()
+    {
+        yield return new object[] { new int[] { 3, 1, 5, 2, 4 }, new int[] { 5, 4, 3, 2, 1 } };
+        yield return new object[] { new int[] { 10, 4, 0, 6, 8, 2 }, new int[] { 10, 8, 6, 4, 2, 0 } };
+        yield return new object[] { new int[] { 87, 13, -89, 0, 42, -176 }, new int[] { 87, 42, 13, 0, -89, -176 } };
+        yield return new object[] { new int[] { -3, -1, 0, -5, -2, -4 }, new int[] { 0, -1, -2, -3, -4, -5 } };
+        yield return new object[] { new int[] { 5, 5, 5, 5, 5 }, new int[] { 5, 5, 5, 5, 5 } };
+        yield return new object[] { new int[] { 3, 7, 3, 6, 3, 5, 3 }, new int[] { 7, 6, 5, 3, 3, 3, 3 } };
+        yield return new object[] { new List<int>(), new List<int>() };
+        yield return new object[] { Array.Empty<int>(), Array.Empty<int>() };
+        yield return new object[] { new List<int>() { 42 }, new List<int>() { 42 } };
+        yield return new object[] { new int[] { 5, 4, 3, 2, 1 }, new int[] { 5, 4, 3, 2, 1 } };
+        yield return new object[] { new int[] { 0, -2, -4, -6, -8, -10 }, new int[] { 0, -2, -4, -6, -8, -10 } };
+        yield return new object[] { new int[] { 10, 20, 30, 40, 50 }, new int[] { 50, 40, 30, 20, 10 } };
+        yield return new object[] { new int[] { int.MinValue, 0, int.MaxValue }, new int[] { int.MaxValue, 0, int.MinValue } };
+        yield return new object[] { Enumerable.Range(0, 100), Enumerable.Range(0, 100).Reverse() };
     }
 }
